da0209c6cd33e8cc8fc7e23a97e4782129853845
natieboy <natieboysupercool@gmail.com>
2025-10-30 00:34:36 +0100

    Fixed tilled dirt saving problem #28
    Fixed! Problem was that I didnt attach the script for one of the saving comonents to its scene, so it wasnt working properly

--- scripts/globals/scene_manager.gd
+++ scripts/globals/scene_manager.gd
@@ -4,8 +4,8 @@ var main_scene_path: String = "res://scenes/main_scene.tscn"
 var main_scene_root_path: String = "/root/MainScene"
 var main_scene_level_root_path: String = "/root/MainScene/GameRoot/LevelRoot"
 
-var level_scenes: Dictionary = {
-	"level1" : "res://scenes/levels/level_1.tscn"
+var level_scenes : Dictionary = {
+	"Level1" : "res://scenes/levels/level_1.tscn"
 }
 
 func load_main_scene_container() -> void:
@@ -16,6 +16,7 @@ func load_main_scene_container() -> void:
 	
 	if node != null:
 		get_tree().root.add_child(node)
+
 
 func load_level(level: String) -> void:
 	var scene_path: String = level_scenes.get(level)
